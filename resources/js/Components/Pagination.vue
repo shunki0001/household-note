f66cf4a527019887af4165607c13e4123ed19fcf
shunki0001 <arkk0807@gmail.com>
2025-07-01 22:47:22 +0900

    ページネーションボタンを日本語化

---
+++ resources/js/Components/Pagination.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { Link } from '@inertiajs/vue3'
+
+defineProps = ({
+    links: Array
+})
+</script>
+
+<template>
+    <div class="mt-4 flex justify-center" v-if="links.length > 3">
+        <nav>
+            <ul class="inline-flex -space-x-px">
+                <li v-for="link in links" :key="link.label">
+                    <Link
+                        v-if="link.url"
+                        :href="link.url"
+                        class="px-3 py-1 rounded-md text-gray-700 bg-white border border-gray-300"
+                        :class="{ 'bg-blue-500 text-white': link.active }"
+                    >
+                        <span v-if="link.label === 'pagination.previous'">&lt; 前へ</span>
+                        <span v-else-if="link.label === 'pagination.next'">次へ &gt;</span>
+                        <span v-else>{{ link.label }}</span>
+                    </Link>
+                    <span
+                        v-else
+                        class="px-3 py-1 rounded-md text-gray-400 bg-white border border-gray-300"
+                    >
+                        <span v-if="link.label === 'pagination.previous'">&lt; 前へ</span>
+                        <span v-else-if="link.label === 'pagination.next'">次へ &gt;</span>
+                        <span v-else>{{ link.label }}</span>
+                </span>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -9,6 +9,8 @@ import { Head, Link, useForm, usePage } from '@inertiajs/vue3';
 import Swal from 'sweetalert2';
 import { watch, onMounted  } from 'vue';
 import Toast from '@/Components/Toast.vue';
+import Pagination from '@/Components/Pagination.vue';
+
 
 const props = defineProps({
     // expenses: Array,
@@ -163,65 +165,68 @@ onMounted(() => {
         </div>
 
         <!-- 一覧表示部分 -->
-         <!-- <pre>{{ expenses }}</pre> -->
-         <div class="py-12">
+        <!-- <pre>{{ expenses }}</pre> -->
+        <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div class="p-6 text-gray-900">
-                    <table class="table-auto w-full">
-                        <thead>
-                            <tr>
-                                <th class="px-4 py-2">金額</th>
-                                <th class="px-4 py-2">日付</th>
-                                <th class="px-4 py-2">費用名</th>
-                                <th class="px-4 py-2">カテゴリー</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="expense in expenses.data" :key="expense.id">
-                                <td class="border px-4 py-2">{{ expense.amount }}</td>
-                                <td class="border px-4 py-2">{{ expense.date }}</td>
-                                <td class="border px-4 py-2">{{ expense.title }}</td>
-                                <td class="border px-4 py-2">{{ expense.category }}</td>
-                                <td class="border px-4 py-2">
-                                    <!-- 要修正->コンポーネント化 -->
-                                     <!-- 編集ボタン -->
-                                    <Link :href="route('expenses.edit', expense.id)" class="text-blue-500 hover:underLine">編集</Link>
-                                    <DeleteButton :expenseId="expense.id"/>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <!-- ページネーションリンクボタン -->
-                    <div class="mt-4 flex justify-center">
-                        <nav>
-                            <ul class="inline-flex -space-x-px">
-                                <li v-for="link in expenses.links" :key="link.label">
-                                    <Link
-                                        v-if="link.url"
-                                        :href="link.url"
-                                        class="px-3 py-1 rounded-md text-gray-700 bg-white border border-gray-300"
-                                        :class="{ 'bg-blue-500 text-white': link.active }"
-                                    >
-                                        <!-- ラベルを判定して日本語に変換 -->
-                                        <span v-if="link.label === 'pagination.previous'">&lt; 前へ</span>
-                                        <span v-else-if="link.label === 'pagination.next'">次へ &gt;</span>
-                                        <span v-else>{{ link.label }}</span>
-                                    </Link>
-                                    <span
-                                        v-else
-                                        class="px-3 py-1 rounded-md text-gray-400 bg-white border border-gray-300"
-                                    >
-                                        <span v-if="link.label === 'pagination.previous'">&lt; 前へ</span>
-                                        <span v-else-if="link.label === 'pagination.next'">次へ &gt;</span>
-                                        <span v-else>{{ link.label }}</span>
-                                    </span>
-                                </li>
-                            </ul>
-                        </nav>
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        <table class="table-auto w-full">
+                            <thead>
+                                <tr>
+                                    <th class="px-4 py-2">金額</th>
+                                    <th class="px-4 py-2">日付</th>
+                                    <th class="px-4 py-2">費用名</th>
+                                    <th class="px-4 py-2">カテゴリー</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="expense in expenses.data" :key="expense.id">
+                                    <td class="border px-4 py-2">{{ expense.amount }}</td>
+                                    <td class="border px-4 py-2">{{ expense.date }}</td>
+                                    <td class="border px-4 py-2">{{ expense.title }}</td>
+                                    <td class="border px-4 py-2">{{ expense.category }}</td>
+                                    <td class="border px-4 py-2">
+                                        <!-- 要修正->コンポーネント化 -->
+                                        <!-- 編集ボタン -->
+                                        <Link :href="route('expenses.edit', expense.id)" class="text-blue-500 hover:underLine">編集</Link>
+                                        <DeleteButton :expenseId="expense.id"/>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <!-- ページネーションリンクボタン -->
+                        <div class="mt-4 flex justify-center">
+                            <nav>
+                                <ul class="inline-flex -space-x-px">
+                                    <li v-for="link in expenses.links" :key="link.label">
+                                        <Link
+                                            v-if="link.url"
+                                            :href="link.url"
+                                            class="px-3 py-1 rounded-md text-gray-700 bg-white border border-gray-300"
+                                            :class="{ 'bg-blue-500 text-white': link.active }"
+                                        >
+                                            <!-- ラベルを判定して日本語に変換 -->
+                                            <span v-if="link.label === 'pagination.previous'">&lt; 前へ</span>
+                                            <span v-else-if="link.label === 'pagination.next'">次へ &gt;</span>
+                                            <span v-else>{{ link.label }}</span>
+                                        </Link>
+                                        <span
+                                            v-else
+                                            class="px-3 py-1 rounded-md text-gray-400 bg-white border border-gray-300"
+                                        >
+                                            <span v-if="link.label === 'pagination.previous'">&lt; 前へ</span>
+                                            <span v-else-if="link.label === 'pagination.next'">次へ &gt;</span>
+                                            <span v-else>{{ link.label }}</span>
+                                        </span>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
                     </div>
                 </div>
+
             </div>
-         </div>
+        </div>
 
     </AuthenticatedLayout>
 </template>
