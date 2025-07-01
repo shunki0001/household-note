87836fde5e388e599a3b318d41bdce0652fcc19e
shunki0001 <arkk0807@gmail.com>
2025-07-01 23:01:16 +0900

    ページネーションコンポーネント化

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -195,33 +195,7 @@ onMounted(() => {
                             </tbody>
                         </table>
                         <!-- ページネーションリンクボタン -->
-                        <div class="mt-4 flex justify-center">
-                            <nav>
-                                <ul class="inline-flex -space-x-px">
-                                    <li v-for="link in expenses.links" :key="link.label">
-                                        <Link
-                                            v-if="link.url"
-                                            :href="link.url"
-                                            class="px-3 py-1 rounded-md text-gray-700 bg-white border border-gray-300"
-                                            :class="{ 'bg-blue-500 text-white': link.active }"
-                                        >
-                                            <!-- ラベルを判定して日本語に変換 -->
-                                            <span v-if="link.label === 'pagination.previous'">&lt; 前へ</span>
-                                            <span v-else-if="link.label === 'pagination.next'">次へ &gt;</span>
-                                            <span v-else>{{ link.label }}</span>
-                                        </Link>
-                                        <span
-                                            v-else
-                                            class="px-3 py-1 rounded-md text-gray-400 bg-white border border-gray-300"
-                                        >
-                                            <span v-if="link.label === 'pagination.previous'">&lt; 前へ</span>
-                                            <span v-else-if="link.label === 'pagination.next'">次へ &gt;</span>
-                                            <span v-else>{{ link.label }}</span>
-                                        </span>
-                                    </li>
-                                </ul>
-                            </nav>
-                        </div>
+                        <Pagination :links="expenses.links"/>
                     </div>
                 </div>
 
