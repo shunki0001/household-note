59fc80ab9479a96f59072446406b6e63e4170fd0
shunki0001 <arkk0807@gmail.com>
2025-08-02 01:44:50 +0900

    Registerバリデーション設定

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -5,6 +5,7 @@ import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
 import { Head, Link, useForm } from '@inertiajs/vue3';
+import { reactive } from 'vue';
 
 const form = useForm({
     name: '',
@@ -13,9 +14,87 @@ const form = useForm({
     password_confirmation: '',
 });
 
+// カスタムバリデーションエラーを管理
+const customErrors = reactive({
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: ''
+});
+
+// バリデーション関数
+const validateForm = () => {
+    let isValid = true;
+
+    // エラーメッセージをリセット
+    customErrors.name = '';
+    customErrors.email = '';
+    customErrors.password = '';
+    customErrors.password_confirmation = '';
+
+    // 名前のバリデーション
+    if (!form.name || form.name.toString().trim() === '') {
+        customErrors.name = '名前を入力して下さい';
+        isValid = false;
+    }
+
+    // メールアドレスのバリデーション
+    if (!form.email || form.email.toString().trim() === '') {
+        customErrors.email = 'メールアドレスを入力して下さい';
+        isValid = false;
+    } else {
+        // メールアドレスの形式チェック
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(form.email)) {
+            customErrors.email = '正しいメールアドレスの形式で入力して下さい';
+            isValid = false;
+        }
+    }
+
+    // パスワードのバリデーション
+    if (!form.password || form.password.toString().trim() === '') {
+        customErrors.password = 'パスワードを入力して下さい';
+        isValid = false;
+    } else if (form.password.length < 8) {
+        customErrors.password = 'パスワードは8文字以上で入力して下さい';
+        isValid = false;
+    }
+
+    // パスワード確認のバリデーション
+    if (!form.password_confirmation || form.password_confirmation.toString().trim() === '') {
+        customErrors.password_confirmation = 'パスワード確認を入力して下さい';
+        isValid = false;
+    } else if (form.password !== form.password_confirmation) {
+        customErrors.password_confirmation = 'パスワードが一致しません';
+        isValid = false;
+    }
+
+    return isValid;
+};
+
 const submit = () => {
+    // カスタムバリデーションを実行
+    if (!validateForm()) {
+        return; // バリデーションエラーがある場合は送信を中止
+    }
+
     form.post(route('register'), {
         onFinish: () => form.reset('password', 'password_confirmation'),
+        onError: (errors) => {
+            // サーバーからのエラーをカスタムエラーに設定
+            if (errors.name) {
+                customErrors.name = errors.name;
+            }
+            if (errors.email) {
+                customErrors.email = errors.email;
+            }
+            if (errors.password) {
+                customErrors.password = errors.password;
+            }
+            if (errors.password_confirmation) {
+                customErrors.password_confirmation = errors.password_confirmation;
+            }
+        },
     });
 };
 </script>
@@ -33,12 +112,11 @@ const submit = () => {
                     type="text"
                     class="mt-1 block w-full"
                     v-model="form.name"
-                    required
                     autofocus
                     autocomplete="name"
                 />
 
-                <InputError class="mt-2" :message="form.errors.name" />
+                <InputError class="mt-2" :message="customErrors.name || form.errors.name" />
             </div>
 
             <div class="mt-4">
@@ -49,11 +127,10 @@ const submit = () => {
                     type="email"
                     class="mt-1 block w-full"
                     v-model="form.email"
-                    required
                     autocomplete="username"
                 />
 
-                <InputError class="mt-2" :message="form.errors.email" />
+                <InputError class="mt-2" :message="customErrors.email || form.errors.email" />
             </div>
 
             <div class="mt-4">
@@ -64,11 +141,10 @@ const submit = () => {
                     type="password"
                     class="mt-1 block w-full"
                     v-model="form.password"
-                    required
                     autocomplete="new-password"
                 />
 
-                <InputError class="mt-2" :message="form.errors.password" />
+                <InputError class="mt-2" :message="customErrors.password || form.errors.password" />
             </div>
 
             <div class="mt-4">
@@ -82,13 +158,12 @@ const submit = () => {
                     type="password"
                     class="mt-1 block w-full"
                     v-model="form.password_confirmation"
-                    required
                     autocomplete="new-password"
                 />
 
                 <InputError
                     class="mt-2"
-                    :message="form.errors.password_confirmation"
+                    :message="customErrors.password_confirmation || form.errors.password_confirmation"
                 />
             </div>
 
