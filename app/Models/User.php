50de3f2f0c34efc1a82304501ffa8461b98c952a
shunki0001 <arkk0807@gmail.com>
2025-08-11 01:46:51 +0900

    登録エラーの不具合を修正

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(\App\Models\Expense::class);
     }
+
+    public function incomes()
+    {
+        return $this->hasMany(\App\Models\Income::class);
+    }
 }
