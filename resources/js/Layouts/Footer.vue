91e012730c25a63e91211487833c52d1fac23e46
shunki0001 <arkk0807@gmail.com>
2025-10-31 11:56:57 +0900

    Footerの未実装の項目を削除

--- resources/js/Layouts/Footer.vue
+++ resources/js/Layouts/Footer.vue
@@ -1,20 +1,72 @@
 <template>
     <footer class="bg-gray-100 border-t py-8 text-800">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div class="grid grid-cols-1 md:grid-cols-4 gap-8">
+            <!-- アイコン部分中央配列 -->
+            <div class="flex flex-col items-center space-y-6">
+                <!-- ロゴ
+                <img
+                    src="/images/applogo.png"
+                    alt="ロゴ"
+                    class="w-12 h-12 mb-2"
+                    style="width: 50px; height: 50px;"
+                /> -->
+
+                <!-- アイコン+テキスト -->
+                <div class="flex flex-col sm:flex-row items-center justify-center gap-10">
+                    <!-- AppLogo -->
+                    <div class="flex flex-col items-center">
+                        <a href="/dashboard">
+                            <img
+                                src="/images/applogo.png"
+                                alt="ロゴ"
+                                class="w-10 h-10 mb-2"
+                                style="width: 40px; height: 40px;"
+                            />
+                            <p class="text-sm text-gray-600">ホーム</p>
+                        </a>
+                    </div>
+                    <!-- Qiita -->
+                    <div class="felx flex-col items-center">
+                        <a href="https://qiita.com/shunki0001">
+                            <img
+                                src="/images/qiita-icon.png"
+                                alt="Qiitaロゴ"
+                                class="w-10 h-10 mb-2"
+                                style="width: 40px; height: 40px;"
+                            />
+                            <p class="text-sm text-gray-600">制作過程の記事はこちら</p>
+                        </a>
+                    </div>
+                    <!-- GitHub -->
+                    <div class="felx flex-col items-center">
+                        <a href="https://github.com/shunki0001/household-note">
+                            <img
+                                src="/images/github-mark.png"
+                                alt="GitHubロゴ"
+                                class="w-10 h-10 mb-2"
+                                style="width: 40px; height: 40px;"
+                            />
+                            <p class="text-sm text-gray-600">GitHubリンク</p>
+                            </a>
+                    </div>
+                </div>
+
+            </div>
+            <!-- <div class="grid grid-cols-1 md:grid-cols-4 gap-8"> -->
                 <!-- ロゴ + アイコン -->
-                <div class="flex flex-col items-start">
+                <!-- <div class="flex flex-col items-start">
                     <div class="mb-2">
-                        <img src="/images/applogo.png" alt="ロゴ" class="w-12 h-12 mb-4" style="width: 50px; height: 50px;" />
+                        <a href="#"><img src="/images/applogo.png" alt="ロゴ" class="w-12 h-12 mb-4" style="width: 50px; height: 50px;" /></a>
                     </div>
                     <div class="flex gap-3">
                         <img src="/images/qiita-icon.png" alt="Qiitaロゴ" class="w-10 h-10" style="width: 40px; height: 40px;" />
                         <img src="/images/github-mark.png" alt="GitHubロゴ" class="w-10 h-10" style="width: 40px; height: 40px;" />
                     </div>
-                </div>
+                </div> -->
 
                 <!-- メニューリンク -->
-                <div>
+                <!--  未実装なので削除 -->
+                <!-- <div>
                     <ul class="space-y-2">
                         <li><a href="#" class="hover:underline">ホーム</a></li>
                         <li><a href="#" class="hover:underline">機能紹介</a></li>
@@ -22,16 +74,16 @@
                         <li><a href="#" class="hover:underline">よくある質問(FAQ)</a></li>
                         <li><a href="#" class="hover:underline">利用規約/プライバシーポリシー</a></li>
                     </ul>
-                </div>
+                </div> -->
 
                 <!-- 作成者情報 -->
-                <div>
+                <!-- <div>
                     <ul class="space-y-2">
                         <li class="font-semibold">作成者：　◯◯</li>
                         <li><a href="#" class="hover:underline">Githubリンク</a></li>
                     </ul>
-                </div>
-            </div>
+                </div> -->
+            <!-- </div> -->
         </div>
         <!-- コピーライト -->
         <div class="flex items-end justify-center">
