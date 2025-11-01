ac5844c31399e02aa240bcb3d44dcfb1e896ee3e
shunki0001 <arkk0807@gmail.com>
2025-11-02 02:05:44 +0900

    web.phpの整理

--- routes/web.php
+++ routes/web.php
@@ -28,10 +28,6 @@ Route::get('/run-seeder', function () {
     return 'Seeder executed successfully.';
 });
 
-// Route::get('/dashboard', function () {
-//     return Inertia::render('Dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
@@ -44,37 +40,28 @@ Route::middleware('auth')->group(function () {
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    Route::post('/expenses', [ExpenseController::class, 'store'])->name('expenses.store');
-    // Route::delete('/expenses/{expense}', [DashboardController::class, 'destroy'])->name('expenses.destroy');
-    Route::delete('/expenses/{expense}', [ExpenseController::class, 'destroy'])->name('expenses.destroy');
-
-    // Route::get('/expenses/{expense}/edit', [DashboardController::class, 'edit'])->name('expenses.edit');
-    Route::get('/expenses/{expense}/edit', [ExpenseController::class, 'edit'])->name('expenses.edit');
-    // Route::get('/expenses/{id}/edit', [DashboardController::class, 'edit'])->name('expenses.edit');
-    Route::put('/expenses/{expense}', [ExpenseController::class, 'update'])->name('expenses.update');
-    // Route::put('/expenses/{expense}', [ExpenseController::class, 'update'])->name('expenses.update');
-    // Route::get('/expenses/monthly', [ExpenseController::class, 'getMonthlyExpenses']);
-
-    // Route::get('/api/expenses/latest-json', [ExpenseController::class, 'latestJson'])->name('expenses.latestJson');
+    Route::get('/api/incomes/total-monthly-incomes', [DashboardController::class, 'getTotalMonthlyIncomes'])->name('dashboard.totalIncome'); // 合計
     Route::get('/api/dashboard/total-expense', [DashboardController::class, 'getTotalExpense'])->name('dashboard.totalExpense');
+
+    Route::post('/expenses', [ExpenseController::class, 'store'])->name('expenses.store');
+    Route::delete('/expenses/{expense}', [ExpenseController::class, 'destroy'])->name('expenses.destroy');
+    Route::get('/expenses/{expense}/edit', [ExpenseController::class, 'edit'])->name('expenses.edit');
+    Route::put('/expenses/{expense}', [ExpenseController::class, 'update'])->name('expenses.update');
+
+    Route::post('/incomes', [IncomeController::class, 'store'])->name('incomes.store'); // 登録
+    Route::put('/incomes/{income}', [IncomeController::class, 'update'])->name('incomes.update'); // 更新
+    Route::get('/incomes/{income}/edit', [IncomeController::class, 'edit'])->name('incomes.edit'); // 編集
+    Route::delete('/incomes/{income}', [IncomeController::class, 'destroy'])->name('incomes.destroy'); // 削除
+
     Route::get('/api/chart-data', [ChartController::class, 'getMonthlyExpenseTotals']);
     Route::get('/api/chart-data/category-monthly-single', [ChartController::class, 'getCategoryExpenseTotals']);
     Route::get('/api/chart-data/doughnut', [ChartController::class, 'doughnutGetCategoryExpenseTotals']);
-    // Route::get('/api/chart-data/monthly-expenses', [ExpenseController::class, 'getMonthlyExpenses']);
 
     // ページ移動
     Route::get('/list', [PageController::class, 'list'])->name('list');
     Route::get('/graph/monthly', [PageController::class, 'monthlyGraph'])->name('graph.monthly');
     Route::get('/graph/category', [PageController::class, 'categoryGraph'])->name('graph.category');
 
-    // 収入関連ルート、API
-    Route::post('/incomes', [IncomeController::class, 'store'])->name('incomes.store'); // 登録
-    Route::put('/incomes/{income}', [IncomeController::class, 'update'])->name('incomes.update'); // 更新
-    // Route::put('/incomes/{id}', [IncomeController::class, 'update'])->name('incomes.update'); // 更新
-    Route::get('/incomes/{income}/edit', [IncomeController::class, 'edit'])->name('incomes.edit'); // 編集
-    // Route::get('/incomes/{id}/edit', [IncomeController::class, 'edit'])->name('incomes.edit'); // 編集
-    Route::delete('/incomes/{income}', [IncomeController::class, 'destroy'])->name('incomes.destroy'); // 削除
-    Route::get('/api/incomes/total-monthly-incomes', [DashboardController::class, 'getTotalMonthlyIncomes'])->name('dashboard.totalIncome'); // 合計
     Route::get('/api/report-data/latest-json', [ReportController::class, 'latestJson'])->name('transaction.latestJson'); // 支出+支出一覧(5件)
     Route::get('/api/report-data/monthly-transactions', [ReportController::class, 'getMonthlyTransactions']); // 支出+支出一覧(月別)
 
